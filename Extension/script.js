295f637dc087a6a0e8cb5b124594e1cc30b3522d
DragoNext <49029552+DragoNext@users.noreply.github.com>
2022-10-31 08:37:49 +0000

    Faster AQWIKI !
    Yup instead of comparing each item to each entry compare fewer entries and check if it is in items :V

--- Extension/script.js
+++ Extension/script.js
@@ -10,24 +10,13 @@ var isChromium = window.chrome;
 
 
 if (document.URL == "https://account.aq.com/AQW/Inventory") {
-	// Add some visual to show that extension is trying to read inventory
-	
-	
-	//
-	
-	
-	// *Change this timeout approach to:
-	// Detect when table is loaded [ table id="listinvFull" ]
 	waitForElement()
-	
 	function inv_init(){
 		var indicator = document.createElement("div")
 		indicator.innerHTML = "<h>Loaded 0 Items</h>"
 		indicator.style = "display: block;width: auto;text-align: right;position:relative;"
 		indicator.classList.add("tblHeader")
-		
 		document.getElementById("listinvFull_filter").appendChild(indicator)
-		
 		var inventoryElement = document.getElementsByTagName("td");
 		var l = 0 
 		for (var x = 0; x < inventoryElement.length; x++) {
@@ -50,15 +39,15 @@ if (document.URL == "https://account.aq.com/AQW/Inventory") {
 		}
 		indicator.innerHTML = "<h>Loaded "+Items.length+" Items</h>"
 		
-		chrome.storage.sync.set({ "aqwitems": Items }, function(){});
 		chrome.storage.local.set({"aqwitems": Items}, function() {});
+		chrome.storage.local.set({"aqwwhere": Where}, function() {});
+		chrome.storage.local.set({"aqwtype": Type}, function() {});
+		chrome.storage.local.set({"aqwbuy": Buy}, function() {});
+		chrome.storage.local.set({"aqwcategory": Category}, function() {});
 		
 	  };
-
-
 	function waitForElement(){
 		if(typeof document.getElementById("listinvFull").innerHTML.length !== "undefined"){
-			//variable exists, do what you want
 			if(document.getElementById("listinvFull").innerHTML.length >= 2000){
 				inv_init();	
 			}
@@ -70,113 +59,31 @@ if (document.URL == "https://account.aq.com/AQW/Inventory") {
 			setTimeout(waitForElement, 250);
 		}
 	}
-	
-	
-
 } else {
-	// Add some visual to represent that extension is working 
+	let top_bar = document.getElementById("page-title")
+	var found_info = document.createElement("a") 
+	found_info.innerHTML = "- Found 0 Items"
+	found_info.style = "font-weight: bold;color:green;"
+	top_bar.appendChild(found_info)
 	
+	var nodeList = document.querySelectorAll("a")
+	const arrayOffset = 200 
+	let arrayList = Array.from(nodeList).slice(arrayOffset) // About 200 is alright
 	
-	//
-	
-	
-	function findNodeByInnerHTML(nodelist, innerHTML){
-    for(let ii = 0; ii < nodelist.length; ii++){
-        if(nodelist[ii].innerHTML === innerHTML)
-            return nodelist[ii]
-		}
-	}
+	var found = 0 
 	window.addEventListener('load', function () {
 	  setTimeout(() => {  console.log("Hi!"); 
 		chrome.storage.local.get({aqwitems: []}, function(result){
 				Items = result.aqwitems;
-	
-				// Preety if nesting <3 
-				// Change nesting to just replacing tags in > ( ) to nothing and then comparing 
-				// or 
-				// retrive more information from account, [Category, Buy, Type] still wouldn't be able to diffrenaite (FREE) (AC) with (MERGE) (AC) :/ shucks
-				for (k in Items) {
-					let span = findNodeByInnerHTML(document.querySelectorAll('a'), Items[k])
-					if (span != undefined) {
-						
-						
-						// Add customization in extension setting 
-						span.style ="font-weight: bold;color:green;";
-						
-						
-					}
-					else {
-						let span = findNodeByInnerHTML(document.querySelectorAll('a'), Items[k]+" (0 AC)")
-						if (span != undefined) {
-							span.style ="font-weight: bold;color:green;";
-						}
-						else {
-							let span = findNodeByInnerHTML(document.querySelectorAll('a'), Items[k]+" (Merge)")
-							if (span != undefined) {
-								span.style ="font-weight: bold;color:green;";
-							}
-							else {
-								let span = findNodeByInnerHTML(document.querySelectorAll('a'), Items[k]+" (Non-AC)")
-								if (span != undefined) {
-									span.style ="font-weight: bold;color:green;";
-								}
-							else {
-								let span = findNodeByInnerHTML(document.querySelectorAll('a'), Items[k]+" (Class)")
-								if (span != undefined) {
-									span.style ="font-weight: bold;color:green;";
-								}
-							else {
-								let span = findNodeByInnerHTML(document.querySelectorAll('a'), Items[k]+" (Class) (AC)")
-								if (span != undefined) {
-									span.style ="font-weight: bold;color:green;";
-								}
-							else {
-								let span = findNodeByInnerHTML(document.querySelectorAll('a'), Items[k]+" (AC)")
-								if (span != undefined) {
-									span.style ="font-weight: bold;color:green;";
-								}
-							
-							else {
-								let span = findNodeByInnerHTML(document.querySelectorAll('a'), Items[k]+" (Class) (Merge)")
-								if (span != undefined) {
-									span.style ="font-weight: bold;color:green;";
-								}
-							else {
-								let span = findNodeByInnerHTML(document.querySelectorAll('a'), Items[k]+" (Quest)")
-								if (span != undefined) {
-									span.style ="font-weight: bold;color:green;";
-								}
-							else {
-								let span = findNodeByInnerHTML(document.querySelectorAll('a'), Items[k]+" (Non-Legend)")
-								if (span != undefined) {
-									span.style ="font-weight: bold;color:green;";
-								}
-							else {
-								let span = findNodeByInnerHTML(document.querySelectorAll('a'), Items[k]+" (Legend)")
-								if (span != undefined) {
-									span.style ="font-weight: bold;color:green;";
-								}
-							
-							
-						}
-							
-						}
-							
-						}	
-							
-						}	
-						}	
-							
-						}	
-							
-						}	
-							
-						}	
-							
-						}	
+				
+				for (var x = 0; x < arrayList.length; x++) {
+					let nodeText = nodeList[arrayOffset+x].innerHTML.split("(")[0] // Removes any (Legend) (Non Ac) Etc if you have one you have one 
+					if (Items.includes(nodeText)) {
+						nodeList[arrayOffset+x].style = "font-weight: bold;color:green;"
+						found += 1 
 					}
 				}
-				
+				found_info.innerHTML = "- Found "+found+" Items"
 				
 			});
 			
