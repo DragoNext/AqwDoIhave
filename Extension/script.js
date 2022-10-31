a26659ce31338b9a03079e696aef9b584a00abc1
DragoNext <49029552+DragoNext@users.noreply.github.com>
2022-10-31 10:56:30 +0000

    Wow Icons if it is in Bank or Inv!
    Shows Icon next to item showing if it's in bank or inv, some resources parsing added so amount also is counted.

--- Extension/script.js
+++ Extension/script.js
@@ -8,6 +8,9 @@ var Category = [];
 
 var isChromium = window.chrome;
 
+const bank_icon = "https://i.imgur.com/3jDQEc0.png"
+const inv_icon = "https://i.imgur.com/dicssH5.png"
+
 
 if (document.URL == "https://account.aq.com/AQW/Inventory") {
 	waitForElement()
@@ -21,16 +24,34 @@ if (document.URL == "https://account.aq.com/AQW/Inventory") {
 		var l = 0 
 		for (var x = 0; x < inventoryElement.length; x++) {
 			l = l + 1 
+			let iterated = inventoryElement[x].innerHTML
 			if (l == 1) {
-				Items.push(inventoryElement[x].innerHTML);
+				Items.push(iterated);
 			} else if (l == 2) {
-				Type.push(inventoryElement[x].innerHTML);
+				Type.push(iterated);
 			} else if (l == 3) {
-				Where.push(inventoryElement[x].innerHTML);
+				Where.push(iterated);
 			} else if (l == 4) {
-				Buy.push(inventoryElement[x].innerHTML);
+				Buy.push(iterated);
 			} else if (l == 5) {
-				Category.push(inventoryElement[x].innerHTML);
+				
+				if (Category == "Item" || Category == "Resource" || Category == "Quest Item") {
+					let itemname = Items.pop(); 
+					if (itemname.includes(" x")) {
+						Category.push([iterated,itemname.split(" x")[1]]); // Correct amount of items 
+						Items.push(itemname.split(" x")[0]); // Correct rescource name 
+					
+					}
+					else {
+						Category.push([iterated, 1]); // Only 1 item avaliable
+						Items.push(itemname);
+					}
+				
+				}
+				else {
+					Category.push(inventoryElement[x].innerHTML);
+				}
+				
 			} else {
 				if (l == 6) {
 					l = 0 
@@ -60,41 +81,60 @@ if (document.URL == "https://account.aq.com/AQW/Inventory") {
 		}
 	}
 } else {
-	let top_bar = document.getElementById("page-title")
+	let Title = document.getElementById("page-title")
 	var found_info = document.createElement("a") 
 	found_info.innerHTML = "- Found 0 Items"
 	found_info.style = "font-weight: bold;color:green;"
-	top_bar.appendChild(found_info)
+	Title.appendChild(found_info)
 	
 	var nodeList = document.querySelectorAll("a")
 	const arrayOffset = 200 
 	let arrayList = Array.from(nodeList).slice(arrayOffset) // About 200 is alright
 	
 	var found = 0 
-	window.addEventListener('load', function () {
-	  setTimeout(() => {  console.log("Hi!"); 
-		chrome.storage.local.get({aqwitems: []}, function(result){
-				Items = result.aqwitems;
+
+	chrome.storage.local.get({aqwbuy: []}, function(result){
+		Buy = result.aqwbuy;
+	});
+	chrome.storage.local.get({aqwcategory: []}, function(result){
+		Category = result.aqwcategory;
+	});
+	chrome.storage.local.get({aqwwhere: []}, function(result){
+		Where = result.aqwwhere;
+	});
+	chrome.storage.local.get({aqwtype: []}, function(result){
+		Type = result.aqwtype;
+	});
+	
+	chrome.storage.local.get({aqwitems: []}, function(result){
+			Items = result.aqwitems;
+			if (Items.includes(Title.innerHTML.split('\n')[1])) {
 				
-				for (var x = 0; x < arrayList.length; x++) {
-					let nodeText = nodeList[arrayOffset+x].innerHTML.split("(")[0] // Removes any (Legend) (Non Ac) Etc if you have one you have one 
-					let nodeLink = nodeList[arrayOffset+x].href
-					let isRep = nodeLink.includes("-faction") // Skip Ranks in merge shop from checking 
-					isRep = !isRep
-					if (isRep) { 
-						if (Items.includes(nodeText)) {
-							nodeList[arrayOffset+x].style = "font-weight: bold;color:green;"
-							found += 1 
-							
+				Title.style = "font-weight: bold;color:green;"
+				
+			};
+			
+			
+			for (var x = 0; x < arrayList.length; x++) {
+				let nodeText = nodeList[arrayOffset+x].innerHTML.split("(")[0] // Removes any (Legend) (Non Ac) Etc if you have one you have one 
+				let nodeLink = nodeList[arrayOffset+x].href
+				let isRep = nodeLink.includes("-faction") // Skip Ranks in merge shop from checking 
+				isRep = !isRep
+				if (isRep) { 
+					if (Items.includes(nodeText)) {
+						nodeList[arrayOffset+x].style = "font-weight: bold;color:green;"
+						if (Where[Items.indexOf(nodeText)] == "Bank") {
+							nodeList[arrayOffset+x].innerHTML = nodeList[arrayOffset+x].innerHTML  + "</a> <img title='In Bank' style='height:20px' src='"+bank_icon+"'></img>"
 						}
+						else {
+							nodeList[arrayOffset+x].innerHTML = nodeList[arrayOffset+x].innerHTML  + "</a> <img title='In inventory' style='height:20px' src='"+inv_icon+"'></img>"
+						}
+						found += 1 
+						
 					}
 				}
-				found_info.innerHTML = "- Found "+found+" Items"
-				
+			}
+			found_info.innerHTML = "- Found "+found+" Items"
 			});
-			
-		
-			
-	}, 0);});
-	
+
 }
