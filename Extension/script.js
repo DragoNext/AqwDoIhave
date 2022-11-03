2196bc5e714004d2a7ba7d3bd6639846d90af28a
DragoNext <49029552+DragoNext@users.noreply.github.com>
2022-11-03 00:18:04 +0000

    aqwiki items loading faster
    just made use of async, and also preparing for new feature.

--- Extension/script.js
+++ Extension/script.js
@@ -11,6 +11,120 @@ var isChromium = window.chrome;
 const bank_icon = "https://i.imgur.com/3jDQEc0.png"
 const inv_icon = "https://i.imgur.com/dicssH5.png"
 
+async function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+async function item_from(nodeList, arrayOffset, Buy, Category, Where, Type, x) { 
+	isHashtag = !nodeLink.includes("#")
+	isSorting = !nodeLink.includes("sort-by-")
+	
+	let nodeLink = nodeList[arrayOffset+x].href
+	if (isHashtag && isSorting) {
+		if (nodeLink.includes("http://aqwwiki.wikidot.com/")){
+			httpGet(nodeLink);
+		}
+	}
+	
+}
+
+async function tag_item(nodeList, arrayOffset, Buy, Category, Where, Type, x) {
+	// getting text of item + removing not needed text (dosen't compare to inv) 
+	let nodeText = nodeList[arrayOffset+x].innerHTML.replace(" (0 AC)","").replace(" (AC)","").replace(" (Armor)","").replace(" (Legend)","").replace(" (temp)","").replace(" (Temp)","").replace(" (Special)","").replace(" (Misc)","")
+	
+	// New Needed_Amount / Amount_you_have elements 
+	let node_1 = document.createElement("a");
+	let node_2 = document.createElement("a");
+	
+	// link of item /item-name 
+	let nodeLink = nodeList[arrayOffset+x].href
+
+	
+	// is rep if the link is just a link to /X-faction 
+	let isRep = !nodeLink.includes("-faction") // Skip Ranks in merge shop from checking 
+	
+	// if shop is a merge shop 
+	let isMerge = document.URL.includes("merge")
+
+	if (isRep) { 
+		if (Items.includes(nodeText)) {
+			nodeList[arrayOffset+x].style = "font-weight: bold;color:green;"
+			if (Type[Items.indexOf(nodeText)].length == 2) {
+				// gets amount from inventory 
+				let amount = parseInt(Type[Items.indexOf(nodeText)][1] )
+				
+				// Geting location of item drop count or location next to item found
+				if ( isMerge ) { 
+					var count_node = nodeList[arrayOffset+x].nextSibling
+				} else {
+					var count_node = nodeList[arrayOffset+x].parentNode.lastChild
+				}
+				if (count_node.data == undefined ) {
+					var count_node = nodeList[arrayOffset+x].nextSibling
+				}
+				
+				// detection for monster drop that has range 
+				if (count_node.data.includes("-")) {
+					var needed_amount = count_node.data.slice(2).replace(" ","");
+				} else {
+					var needed_amount = parseInt(count_node.data.slice(2).replace(",",""));
+				}
+				
+				// formating original amount / needed amount to final result.
+				if (isNaN(needed_amount)) {
+					var needed_amount=1;
+				};
+				node_1.innerHTML = String(" x"+needed_amount+"/")	
+		
+				
+			
+				// Stylizer for amount of items
+				node_2.innerHTML = String(amount)
+				if (needed_amount <= amount) {
+					node_1.style = "color:black;"
+					node_2.style = "font-weight: bold;color:green;"
+				}
+				else {
+					node_1.style = "color:black;"
+					node_2.style = "font-weight: bold;color:red;"
+				}
+				
+				if (isMerge) {
+					// Dosen't require adding new element (Just replace , with / )
+					count_node.data = count_node.data.replace(",","")+"/"
+					// Ads new element 
+					nodeList[arrayOffset+x].parentNode.insertBefore(node_2, nodeList[arrayOffset+x].nextSibling.nextSibling)
+					
+				}
+				else {
+					// erases previous data 
+					count_node.data = ""
+					
+					// Adds new element 
+					nodeList[arrayOffset+x].parentNode.appendChild(node_1) 
+					nodeList[arrayOffset+x].parentNode.appendChild(node_2) 
+				
+				}
+			}
+			
+			// Adds icons of where is located 
+			if (Where[Items.indexOf(nodeText)] == "Bank") {
+				nodeList[arrayOffset+x].innerHTML = nodeList[arrayOffset+x].innerHTML  + "</a> <img title='In Bank' style='height:20px' src='"+bank_icon+"'></img>"
+			}
+			else {
+				nodeList[arrayOffset+x].innerHTML = nodeList[arrayOffset+x].innerHTML  + "</a> <img title='In inventory' style='height:20px' src='"+inv_icon+"'></img>"
+			}
+			found += 1 //Count items found 
+		}
+	}
+};
+
+
+
 
 if (document.URL == "https://account.aq.com/AQW/Inventory") {
 	waitForElement()
@@ -106,6 +220,8 @@ if (document.URL == "https://account.aq.com/AQW/Inventory") {
 		Type = result.aqwtype;
 	});
 	
+	
+	
 	chrome.storage.local.get({aqwitems: []}, function(result){
 			Items = result.aqwitems;
 			if (Items.includes(Title.innerHTML.split('\n')[1])) {
@@ -116,95 +232,9 @@ if (document.URL == "https://account.aq.com/AQW/Inventory") {
 			
 			
 			for (var x = 0; x < arrayList.length; x++) {
-				// getting text of item + removing not needed text (dosen't compare to inv) 
-				let nodeText = nodeList[arrayOffset+x].innerHTML.replace(" (0 AC)","").replace(" (AC)","").replace(" (Armor)","").replace(" (Legend)","").replace(" (temp)","").replace(" (Temp)","").replace(" (Special)","").replace(" (Misc)","")
-				
-				
-				// New Needed_Amount / Amount_you_have elements 
-				let node_1 = document.createElement("a");
-				let node_2 = document.createElement("a");
-				
-				// link of item /item-name 
-				let nodeLink = nodeList[arrayOffset+x].href
-				
-				// is rep if the link is just a link to /X-faction 
-				let isRep = !nodeLink.includes("-faction") // Skip Ranks in merge shop from checking 
-				
-				// if shop is a merge shop 
-				let isMerge = document.URL.includes("merge")
-			
-				
-				if (isRep) { 
-					if (Items.includes(nodeText)) {
-						nodeList[arrayOffset+x].style = "font-weight: bold;color:green;"
-						if (Type[Items.indexOf(nodeText)].length == 2) {
-							// gets amount from inventory 
-							let amount = parseInt(Type[Items.indexOf(nodeText)][1] )
-							
-							// Geting location of item drop count or location next to item found
-							if ( isMerge ) { 
-								var count_node = nodeList[arrayOffset+x].nextSibling
-							} else {
-								var count_node = nodeList[arrayOffset+x].parentNode.lastChild
-							}
-							if (count_node.data == undefined ) {
-								var count_node = nodeList[arrayOffset+x].nextSibling
-							}
-							
-							// detection for monster drop that has range 
-							if (count_node.data.includes("-")) {
-								var needed_amount = count_node.data.slice(2).replace(" ","");
-							} else {
-								var needed_amount = parseInt(count_node.data.slice(2).replace(",",""));
-							}
-							
-							// formating original amount / needed amount to final result.
-							if (isNaN(needed_amount)) {
-								var needed_amount=1;
-							};
-							node_1.innerHTML = String(" x"+needed_amount+"/")	
-					
-							
-						
-							// Stylizer for amount of items
-							node_2.innerHTML = String(amount)
-							if (needed_amount <= amount) {
-								node_1.style = "color:black;"
-								node_2.style = "font-weight: bold;color:green;"
-							}
-							else {
-								node_1.style = "color:black;"
-								node_2.style = "font-weight: bold;color:red;"
-							}
-							
-							if (isMerge) {
-								// Dosen't require adding new element (Just replace , with / )
-								count_node.data = count_node.data.replace(",","")+"/"
-								// Ads new element 
-								nodeList[arrayOffset+x].parentNode.insertBefore(node_2, nodeList[arrayOffset+x].nextSibling.nextSibling)
-								
-							}
-							else {
-								// erases previous data 
-								count_node.data = ""
-								
-								// Adds new element 
-								nodeList[arrayOffset+x].parentNode.appendChild(node_1) 
-								nodeList[arrayOffset+x].parentNode.appendChild(node_2) 
-							
-							}
-						}
-						
-						// Adds icons of where is located 
-						if (Where[Items.indexOf(nodeText)] == "Bank") {
-							nodeList[arrayOffset+x].innerHTML = nodeList[arrayOffset+x].innerHTML  + "</a> <img title='In Bank' style='height:20px' src='"+bank_icon+"'></img>"
-						}
-						else {
-							nodeList[arrayOffset+x].innerHTML = nodeList[arrayOffset+x].innerHTML  + "</a> <img title='In inventory' style='height:20px' src='"+inv_icon+"'></img>"
-						}
-						found += 1 //Count items found 
-					}
-				}
+				tag_item(nodeList, arrayOffset, Buy, Category, Where, Type, x) 
+				// item_from(nodeList, arrayOffset, Buy, Category, Where, Type, x)
+				// New function 
 			}
 			found_info.innerHTML = "- Found "+found+" Items" // Displays items found 
 			});
