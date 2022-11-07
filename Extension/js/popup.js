44ec9afa87daab3b7b6b308233bd8f209922df6e
DragoNext <49029552+DragoNext@users.noreply.github.com>
2022-11-07 14:34:30 +0000

    Fixes to options and popup

--- Extension/js/popup.js
+++ Extension/js/popup.js
@@ -1,6 +1,11 @@
-document.addEventListener('DOMContentLoaded', restore_options);
 
-function restore_options() {
+if (document.URL.includes("chrome-extension")) { 
+	document.addEventListener('DOMContentLoaded', popup_restore_options);
+}
+else{
+
+}
+function popup_restore_options() {
 	var docs = document.getElementById('dark_moded')
 	chrome.storage.local.get({darkmode: 0}, function(result){
 	if (result.darkmode == 1) {
@@ -14,7 +19,7 @@ function restore_options() {
  })
  }
  
-function save_options() {
+function popup_save_options() {
   var Dark_Mode = 0
   if (document.getElementById('dark_moded').innerHTML == "Dark Mode (ON)") {
 	  Dark_Mode = 0
@@ -22,9 +27,10 @@ function save_options() {
 	  Dark_Mode = 1
   }
   chrome.storage.local.set({"darkmode": Dark_Mode}, function() {});
-  restore_options()
+  popup_restore_options()
   }
  
- 
- document.getElementById('dark_moded').addEventListener('click',
-    save_options);
+ try {
+ 	document.getElementById('dark_moded').addEventListener('click',
+    		popup_save_options);
+} catch (error) {}
