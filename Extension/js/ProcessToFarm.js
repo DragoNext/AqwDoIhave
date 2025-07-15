f59d71b9a4475c0541b21ac85fe45c82b80793da
Süleyman Batuhan Vatan <sbvatan@hotmail.com>
2025-07-15 14:39:05 +0300

    Fixed tofarm page.

--- Extension/js/ProcessToFarm.js
+++ Extension/js/ProcessToFarm.js
@@ -1,334 +1,260 @@
-// ProcessWikiItem.js
+// Whole Wiki Pre processed using python script 
 
-// Get Json of Wiki Exclusion Suffixes 
-var wiki_exclude_suffixes = getJson(chrome.runtime.getURL("data/wiki_exclude_suffixes.json"))
-var collection_chests = getJson(chrome.runtime.getURL("data/collection_chests.json"))["chests"]
-var items_json = getJson(chrome.runtime.getURL("data/WikiItems.json"))
+// How it looks?
+//
+//	{Name}: [Data]
+//
+//	Data:
+//		0 - Type >> Item/Monster/Location 
+//		1 - href >> Link To Item 
+//      2 - >> 
+var ac_large = "http://aqwwiki.wdfiles.com/local--files/image-tags/aclarge.png"
+var rare_large = "http://aqwwiki.wdfiles.com/local--files/image-tags/rarelarge.png"
+var seasonal_large = "http://aqwwiki.wdfiles.com/local--files/image-tags/seasonallarge.png"
+var legend_large = "http://aqwwiki.wdfiles.com/local--files/image-tags/legendlarge.png"
 
-// WIP stuff 
 
-function isCollection(text) {
-	let value = false 
-	for (var x = 0; x < collection_chests.length; x++) {
-		if (text.includes(collection_chests[x])) {
-			let value = collection_chests[x]
-			return value 
-		}
-	}
-	return value 
-}
 
-function httpGet(theUrl, nodeList, arrayOffset, x, isMonster, isQuest, isMerge)
+function getJson(theUrl)
 {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, true ); 
-	let priceElement = document.createElement("a")
+    xmlHttp.open( "GET", theUrl, false ); 
+	xmlHttp.send(null);
+    return JSON.parse(xmlHttp.responseText)
+}
+
+var items_json = getJson(chrome.runtime.getURL("data/WikiItems.json"))
+var wiki_exclude_suffixes = getJson(chrome.runtime.getURL("data/wiki_exclude_suffixes.json"))
+
+
+
+async function add_to_table(table,item_name,item_details, av_item_count, avaliableItemsElement){
+	let tr = document.createElement("tr") 
+	let td_1 = document.createElement("td")
+	let td_2 = document.createElement("td")
+	let td_3 = document.createElement("td")
+	av_item_count+=1
 	
-	xmlHttp.onreadystatechange = checkData;
-	xmlHttp.send( null );
-	function addElement() {
-		if (isQuest || isMonster || isMerge || (document.location.href == "http://aqwwiki.wikidot.com/new-releases")) {
-			nodeList[arrayOffset+x].prepend(priceElement)
-		} else{
-			nodeList[arrayOffset+x].appendChild(priceElement)
+	td_1.innerHTML = item_name
+	
+	if (item_details[1][1][0] == "Drop" || item_details[1][1][0] == "Quest" || item_details[1][1][0] == "Merge") {
+		if  (item_details[1][1][1] !== "") {
+			td_2.innerHTML = "<a href='http://aqwwiki.wikidot.com/"+item_details[1][1][2]+"'>"+item_details[1][1][1]+"</a>"
+			
+		} else {
+			td_2.innerHTML = item_details[1][1][1][0]
 		}
+	} else {
+		td_2.innerHTML = "N/A"
 	}
+	
+	// Monster Drop Icon
+	if (item_details[1][1][0] == "Drop") {
+		td_3.innerHTML = td_3.innerHTML + "<img style='height:20px' src='"+drop_icon+"'></img>"
+	}
+	// Quest Icon
+	if (item_details[1][1][0] == "Quest") {
+		td_3.innerHTML = td_3.innerHTML + "<img style='height:20px' src='"+quest_icon+"'></img>"
+	}
+	// Merge Icon
+	if (item_details[1][1][0] == "Merge") {
+		td_3.innerHTML = td_3.innerHTML + "<img style='height:20px' src='"+mergeshop_icon+"'></img>"
+	}
+	// Ac Icon
+	if (item_details[6][1] == true) {
+		td_3.innerHTML = td_3.innerHTML + "<img style='height:20px' src='"+ac_large+"'></img>"
+	}
+	// Legend Icon
+	if (item_details[7][1] == true) {
+		td_3.innerHTML = td_3.innerHTML + "<img style='height:20px' src='"+legend_large+"'></img>"
+	}
+	// Seasonal Icon
+	if (item_details[8][1] == true) {
+		td_3.innerHTML = td_3.innerHTML + "<img style='height:20px' src='"+seasonal_large+"'></img>"
+	}
+	
+	tr.appendChild(td_1)
+	tr.appendChild(td_2)
+	tr.appendChild(td_3)
+	table.appendChild(tr)
 
 	
+}
+
+function processToFarmItem(item_name,item_details,table) {
+	add_to_table(table,item_name,item_details)
+}
+
+
 	
-	function checkData() {
+function reProcess_ToFarm_Page() {
+	var table_element = document.getElementById("table-content")
+	table_element.innerHTML = "" 
+	process_ToFarm_Page()
+}
+
+
+
+
+
+
+function filterItem(item_name, item_data, account_items) {
+		// Filters check boxes True/False On/Off
+		var Filter_AcItem = document.getElementById("Filter_AcItem").checked 
+		var Filter_LegendItem = document.getElementById("Filter_LegendItem").checked 
+		var Filter_NormalItem = document.getElementById("Filter_NormalItem").checked 
+		var Filter_SeasonalItem = document.getElementById("Filter_SeasonalItem").checked 
+		var Filter_MonsterDrop = document.getElementById("Filter_MonsterDrop").checked 
+		var Filter_MergeDrop = document.getElementById("Filter_MergeDrop").checked 
+		var Filter_QuestDrop = document.getElementById("Filter_QuestDrop").checked 
 		
-		if (xmlHttp.readyState == 4) {
-			try{
-			var Text = xmlHttp.responseText
-			if (Text.includes(" Gold") || Text.includes(" AC")) {
-				let cchest = isCollection(Text)
-				
-				if (cchest !== false) {
-					price =  Text.split("Location:</strong>")[1].split("</a>")[0]
-					title = "From Collection Chest: "+cchest 
-					href = price.split(">")[0].split('"')[1]
-					
-					priceElement.href = href
-					priceElement.innerHTML = '<img title="'+title+'" style="width:22px" src="'+collectionchest_icon+'"></img>'
-					
-					addElement()
-				}
-				else{
-					var nm = "Location:</strong>"
-					if (Text.includes("Locations:</strong>")) {
-						var nm = "Locations:</strong>"
-					}
-					else{
-						var nm = "Location:</strong>"
-					}
-					price =  Text.split(nm)[1].split("</a>")[0]
-					title = "From Shop: "+price.split(">")[1]
-					href = price.split(">")[0].split('"')[1]
-					priceElement.href = href
-					priceElement.innerHTML = '<img title="'+title+'" style="width:22px" src="'+shop_icon+'"></img>'
-					addElement()
-					
-				}
-				
-			}
-			if (Text.includes('Price:</strong>')) {
-				if (Text.includes("N/A (Reward from the")){
-					if (Text.includes("Open Treasure Chests")) {
-						title = "From Treasure Chest"
-						href = "http://aqwwiki.wikidot.com/twilly-s-quests#2"
-						
-						priceElement.href = href
-						priceElement.innerHTML = '<img title="'+title+'" style="width:22px" src="'+treasurechest_icon+'"></img>'
-						
-						addElement()
-					}
-					else{
-						price =  Text.split("N/A (Reward from the")[1]
-						title = "From Quest: "+ price.split('>')[1].split('<')[0]
-						href = price.split('href="')[1].split('"')[0]
-						
-						priceElement.href = href
-						priceElement.innerHTML = '<img title="'+title+'" style="width:22px" src="'+quest_icon+'"></img>'
-						
-						addElement()
-					}
-				} 
-				if (Text.includes("Reward from the:")){
-					if (Text.includes("Wheel of Doom")){
-						price =  "http://aqwwiki.wikidot.com/wheel-of-doom"
-						title = "Dropped by Wheel of Doom"
-						href = price 
-						
-						priceElement.href = href
-						priceElement.innerHTML = '<img title="'+title+'" style="width:22px" src="'+whellofdoom_icon+'"></img>'
-						
-						addElement()
-					}
-				}
-				if (Text.includes("Merge the following:")) {
-					price =  Text.split("Location:</strong>")[1].split("</a>")[0]
-					title = "From Merge Shop: "+price.split(">")[1]
-					href = price.split(">")[0].split('"')[1]
-					
-					priceElement.href = href
-					priceElement.innerHTML = '<img title="'+title+'" style="width:22px" src="'+mergeshop_icon+'"></img>'
-					
-					addElement()
-				}
-				if (Text.includes("Dropped by")) {
-					
-					price =  Text.split("Price:")[1].split("strong>")[1].split("<br>")[0].replaceAll('"','')
-					
-					if (price !== undefined) {
-						title = "Dropped by "+price.split("</a>")[0].split("N/A (")[1].split(">")[1]
-						href = price 
-						
-						priceElement.href = href.split("href")[1].split(">")[0].replace("=","http://aqwwiki.wikidot.com")
-						priceElement.innerHTML = '<img title="'+title+'" style="width:22px" src="'+drop_icon+'"></img>'
-						
-						
-						
-						addElement()
-						
-					}
-				}
-				
-			}
-			if (Text.includes("Collection Chest")) {
-				price = Text.split("Locations:")[1].split("</a>")[0].replaceAll('"','')
-				if (price !== undefined){
-				
-					title = ""
-					href = ""
-
-					priceElement.href = href
-					priceElement.innerHTML = '<img  title="'+title+'" style="width:22px" src="'+collectionchest_icon+'"></img></div>'
-					addElement()
-				}
-			}
-			
-			delete xmlHttp
-
-			return true
-			
-			} 
-			catch (err) 
-			{
-			if (!err.toString().includes("split")) console.log({ err })
-			return true
-			}
+		
+		// Gets Tag exclusion from wiki_exclude_suffixes.json and make it smaller to compare with account_items 
+		let cq = item_name.toLowerCase()
+		for (var i = 0; i < wiki_exclude_suffixes["Excluded"].length; i++) {
+			cq = cq.replace(wiki_exclude_suffixes["Excluded"][i].toLowerCase(), "")
+		} 
+		
+		
+		
+		// Filter Drop Type on if any is TRUE and ITEM is of type it will return False (Skip Item) 
+		if (item_data[1][1][0] == "Drop" && Filter_MonsterDrop == false) {
+			return false 
 		}
-	}
-}
-
-function getWikiItemDetails(itemName) {
-	let moreDetailIcon = document.createElement("a")
-	
-	items_json[itemName] 
-}
-
-function addItemDetailsIcon(details) {
-	
-}
-
-
-	
-async function ProcessAnyWikiItem(nodeList, arrayOffset, Buy, Category, Where, Type, x, isMonster, isQuest, isMerge) { 
-
-	let nodeText = nodeList[arrayOffset+x].innerHTML; // No need for highlight 
-	let nodeLink = nodeList[arrayOffset+x].href;
-	
-
-	
-	if (nodeLink.includes("http://aqwwiki.wikidot.com/")){
-		httpGet(nodeLink, nodeList, arrayOffset, x, isMonster, isQuest, isMerge);
-	}
-	
-}
-//
-
-
-function processRescourceItem(Items, nodeText, nodeList, arrayOffset, x, isMerge, isQuest, isMonster) {
-	var accountAmount = parseInt(Type[Items.indexOf(nodeText)][1] )
-	var originalAmountCount = document.createElement("span") 
-	var accountAmountCount = document.createElement("span") 
-	
-	if (isMerge || isMonster) {
-		var count_node = nodeList[arrayOffset+x].nextSibling
-		accountAmountCount.classList.add("RescourceAcquired")
-	} else if (isQuest) {
-		var count_node = nodeList[arrayOffset+x].parentNode.lastChild
-	} 
-	// ---- START OF FIX ----
-    if (!count_node) {
-        // If count_node was never found, stop the function here to prevent a crash.
-        return; 
-    }
-    // ---- END OF FIX ----
-	var originaAmount = count_node.data.replace(",","")
-	
-	if (originaAmount !== null && originaAmount !== undefined){ 
-	
-		originalAmountCount.innerHTML = originaAmount 
-		accountAmountCount.innerHTML = accountAmount 
-
-		if (isMonster == false) {
-			if (parseInt(originaAmount.replace("x","")) <= accountAmount) {
-				//stack_original.style = "color:black;
-				accountAmountCount.style = "font-weight: bold;color:green;text-decoration:none;"
+		if (item_data[1][1][0] == "Quest" && Filter_QuestDrop == false) {
+			return false 
+		}
+		if (item_data[1][1][0] == "Merge" && Filter_MergeDrop == false) {
+			return false 
+		}
+		
+		
+		if (item_data[5] != undefined) {
+			// item_data[5] if undefined, the object isn't presentable (Item dosen't have data if its AC)
+			if (account_items.includes(cq)) {
+				
+				return false 
+				
+			} 
+			else if (item_data[14] == "necklaces" || item_data[14] == "misc-items") {
+				// Exclude Misc Items and Necklaces 
+				return false 
+			} else if (item_data[8][1] && Filter_SeasonalItem == false) {
+				// Seasonal Filter 
+				return false 
+			} else if (item_data[6][1] == false && Filter_NormalItem == false) { 
+					// Ac Filter 
+					return false 			
+			} 
+			
+			else if (item_data[5][1] == true) { 
+				// Excludes Rare Tag  
+					return false 			
+			} 
+			
+			else if (item_data[1][1][0] == "Drop" || item_data[1][1][0] == "Quest" || item_data[1][1][0] == "Merge") {
+				if (item_data[1][1][0] == "Merge") {
+					if (item_data[1][1][1].includes("Doom Merge")) {
+						// Ignore Doom Merge 
+						return false 
+					} else {
+						// Normal Merge Drop Item 
+						return true 
+					}
+				} else if (item_data[1][1][0] == "Quest") {
+					if (item_data[1][1][1] == "Open Treasure Chests" || item_data[1][1][1] == "Wheel of Doom") {
+						// Ignore Open Chest 
+						return false 
+					}
+					else{
+						// Normal Quest Drop Item
+						return true 
+					}
+				} else {
+					// Normal Monster Drop Item 
+					return true
+				}
 			}
 			else {
-				//stack_original.style = "color:black;"
-				accountAmountCount.style = "font-weight: bold;color:red;text-decoration:none;"
+				return false 
 			}
-		} else {
-			accountAmountCount.style = "font-weight: bold;color:green;"
 		}
-		
-		
-
-		
-		count_node.data = " "
-		return [originalAmountCount,accountAmountCount]
-	}
-	
-}
-
-async function addLocationIcon(nodeList, nodeText, Items, arrayOffset, x, isList, isMerge) {
-	// where_icon 
-	let where_icon = document.createElement("a");
-	
-	// Adds icons of where is located 	
-	if (Where[Items.indexOf(nodeText)] == "Bank") {
-		where_icon.innerHTML = " <img title='In Bank' style='height:20px' src='"+bank_icon+"'></img>"
-		if (isList) {
-			nodeList[arrayOffset+x].parentNode.appendChild(where_icon, nodeList[arrayOffset+x])
-
-		} else if (isMerge || window.location.href == 'http://aqwwiki.wikidot.com/new-releases'){
-				nodeList[arrayOffset+x].appendChild(where_icon)
-		} 
 		else {
-			nodeList[arrayOffset+x].parentNode.appendChild(where_icon)
+			// Failed to retrive item ._.
+			// Fault of scraper in 99.99% of times.
 		}
-		
-		
-	} else {
-		if (isList) {
-			where_icon.innerHTML = " <img title='In inventory' style='height:20px' src='"+inv_icon+"'></img>"
-			nodeList[arrayOffset+x].parentNode.appendChild(where_icon, nodeList[arrayOffset+x])
-
-		} else {
-			where_icon.innerHTML = " <img title='In inventory' style='height:20px' src='"+inv_icon+"'></img>"
-			nodeList[arrayOffset+x].appendChild(where_icon)
-		}
-	}	
-	
 }
 
+async function process_ToFarm_Page() {
+	
+	// Count of ac - account, and av - avaliable items
+	var av_item_count = 0 
+	var ac_item_count = 0 
+	
+	
+	
+	const item_keys = Object.keys(items_json)
+	
+	var table_element = document.getElementById("table-content")
+	
+	
+	var avaliableItemsElement = document.getElementById("av-items")
+	var accounteItemsElement = document.getElementById("ac-items")
+	avaliableItemsElement.innerHTML = "Avaliable Items: "+av_item_count
+	accounteItemsElement.innerHTML = "Account Items: "+ac_item_count
+	
+	
+	chrome.storage.local.get({aqwitems: []}, function(result){
+		var account_items = result.aqwitems
+		ac_item_count = result.aqwitems.length
+		for (var x = 0; x < item_keys.length; x++) {
 
-
-
-async function ProcessWikiItem(nodeList, arrayOffset, Items, Buy, Category, Where, Type, x, isMerge, isList, isQuest, isMonster) {
-	// getting text of item + removing not needed text (dosen't compare to inv) 
-	let nodeText = nodeList[arrayOffset+x].innerHTML.replace("’","'").trim();
-	
-	
-	// Use Wiki Excluded Suffixes json to remove unused suffixes 
-	for (var i = 0; i < wiki_exclude_suffixes["Excluded"].length; i++) {
-		nodeText = nodeText.replace(wiki_exclude_suffixes["Excluded"][i],"")
-	}
-	nodeText = nodeText.toLowerCase()
-	
-	
-	
-	
-	// link of item /item-name 
-	let nodeLink = nodeList[arrayOffset+x].href
-	
-	// [Edge Case] is rep if the link is just a link to /X-faction not a item 
-	let isRep = !nodeLink.includes("-faction") // Skip Ranks in merge shop from checking 
-	
-	
-	if (isRep) { 
-		if (Items.includes(nodeText)) {
-			nodeText = nodeText
-			nodeList[arrayOffset+x].style = "font-weight: bold;color:green;"
-			nodeList[arrayOffset+x].classList.add("Acquired")
-			if (Type[Items.indexOf(nodeText)].length == 2 && window.location.href !== "http://aqwwiki.wikidot.com/misc-items") {
-				// gets amount from inventory 
-				var RescourceCount = processRescourceItem(Items, nodeText, nodeList, arrayOffset, x, isMerge, isQuest, isMonster);
-			} else {
-				var RescourceCount = false 
+			if (filterItem(item_keys[x], items_json[item_keys[x]], account_items)) {
+				processToFarmItem(item_keys[x], items_json[item_keys[x]],table_element, av_item_count, avaliableItemsElement)
+				av_item_count+= 1
 			}
-			
-			// Adds icons of where is located 	
-			addLocationIcon(nodeList, nodeText, Items, arrayOffset, x, isList, isMerge)
-			
-			
-			
-			if (RescourceCount !== false){
-				var Separator = document.createElement("b") 
-				Separator.innerHTML = "/"
-				if (RescourceCount[0].innerHTML == " "){
-					RescourceCount[0].innerHTML = " x1"
-				}
-				
-				if (isMerge) {
-					
-					nodeList[arrayOffset+x].parentNode.insertBefore(RescourceCount[1], nodeList[arrayOffset+x].nextSibling)
-					nodeList[arrayOffset+x].parentNode.insertBefore(Separator, nodeList[arrayOffset+x].nextSibling)
-					nodeList[arrayOffset+x].parentNode.insertBefore(RescourceCount[0], nodeList[arrayOffset+x].nextSibling)
-				} 
-				else{
-					nodeList[arrayOffset+x].parentNode.appendChild(RescourceCount[0])
-					nodeList[arrayOffset+x].parentNode.appendChild(Separator)
-					nodeList[arrayOffset+x].parentNode.appendChild(RescourceCount[1])
-				}
-			}
-			
-			
-			found += 1 //Count items found 
 		}
-	}
-};
+		avaliableItemsElement.innerHTML = "Avaliable Items: "+av_item_count	
+		accounteItemsElement.innerHTML = "Account Items: "+ac_item_count	
+		
+	})
+}
+const drop_icon = chrome.runtime.getURL("images/monster_drop.png")
+const quest_icon = chrome.runtime.getURL("images/quest_icon.png")
+const mergeshop_icon = chrome.runtime.getURL("images/mergeshop_icon.png")
+
+
+if (window.location.href.includes("tofarm.html")) {
+	
+	document.addEventListener('DOMContentLoaded', function(event) {
+		
+		
+		var dropFilter = document.getElementById("bossdrop")
+		dropFilter.src = drop_icon
+		
+		var dropFilter = document.getElementById("mergeshopdrop")
+		dropFilter.src = mergeshop_icon
+		
+		var dropFilter = document.getElementById("questdrop")
+		dropFilter.src = quest_icon
+		
+		
+		process_ToFarm_Page()
+		
+		document.getElementById('Filter_AcItem').addEventListener('click',
+			reProcess_ToFarm_Page);
+		document.getElementById('Filter_LegendItem').addEventListener('click',
+			reProcess_ToFarm_Page);		
+		document.getElementById('Filter_NormalItem').addEventListener('click',
+			reProcess_ToFarm_Page);
+		document.getElementById('Filter_SeasonalItem').addEventListener('click',
+			reProcess_ToFarm_Page);	
+		document.getElementById('Filter_MonsterDrop').addEventListener('click',
+			reProcess_ToFarm_Page);	
+		document.getElementById('Filter_MergeDrop').addEventListener('click',
+			reProcess_ToFarm_Page);	
+		document.getElementById('Filter_QuestDrop').addEventListener('click',
+			reProcess_ToFarm_Page);
+		
+	})	
+}
